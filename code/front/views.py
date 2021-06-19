cc1061ecaf96d1d40f180956f2a3d45ba246bdd7
Zhu Enzuo <misakihanayo@qq.com>
2021-06-19 22:35:26 +0800

    finish login

--- code/front/views.py
+++ code/front/views.py
@@ -3,7 +3,8 @@ from django.db import connection
 import random
 from django.shortcuts import redirect
 from django.shortcuts import reverse
-
+from . import models
+from .forms import UserForm
 def get_corsor():
     return connection.cursor()
 
@@ -40,3 +41,50 @@ def delete_book(request):
         return redirect(reverse('index'))
     else:
         raise RuntimeError("删除图书的method错误！")
+
+def login(request):
+    if request.method == "POST":
+        login_form = UserForm(request.POST)
+        message = "请检查填写的内容！"
+        if login_form.is_valid():
+            username = login_form.cleaned_data['username']
+            password = login_form.cleaned_data['password']
+            print ("fuck ",username,password)
+            cursor = get_corsor()
+            cursor.execute("select user_password from WebUser where user_name='%s'" % (username))
+            FoundPassword=cursor.fetchall()
+            print(FoundPassword[0][0])
+            try:
+                print("entered try")
+                #FoundPassword=cursor.execute("select user_password from WebUser where user_name='%s'" % (username))
+                if FoundPassword[0][0] == password:
+                    return redirect('/index/')
+                else:
+                    message = "密码不正确！"
+            except:
+                message = "用户不存在！"
+        return render(request, 'login.html', locals())
+
+    login_form = UserForm()
+    return render(request, 'login.html', locals())
+
+
+def register(request):
+    if request.method == "POST":
+        register_form = UserForm(request.POST)
+        message = "请检查填写的内容！"
+        if register_form.is_valid():
+            username = register_form.cleaned_data['username']
+            password = register_form.cleaned_data['password']
+            cursor = get_corsor()
+            cursor.execute("insert into WebUser(user_id,user_name,user_password) values('%d','%s','%s')" % (random.randint(0,1000),username,password))
+            print("execute achieved")
+        return render(request, 'register.html', locals())
+
+    register_form = UserForm()
+    return render(request, 'register.html')
+
+def logout(request):
+    pass
+    return redirect('/index/')
+
